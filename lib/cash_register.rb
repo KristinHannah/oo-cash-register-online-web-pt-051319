d2af0af658988c7c7d1d3ccdb56c9fdffbf0bf98
Kristin Kraemer <kristinhannahk@gmail.com>
2019-06-09 23:42:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,15 +25,6 @@ class CashRegister
     @@cart
   end 
   
-    describe '#items' do
-    it 'returns an array containing all items that have been added' do
-      new_register = CashRegister.new
-      new_register.add_item("eggs", 1.99)
-      new_register.add_item("tomato", 1.76, 3)
-      expect(new_register.items).to eq(["eggs", "tomato", "tomato", "tomato"])
-    end
-  end
-  
   def add_item(title, price, *quantity)
     if quantity != []
       to_add = price * quantity[0].to_s.to_f
